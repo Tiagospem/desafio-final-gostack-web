d00b987b4cb49fc8589d003aa5768224a5911227
Tiagospem <tiagospem@gmail.com>
2019-09-30 17:43:40 -0300

    added logo

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '~/assets/logo.png'
 
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <>
-      <img alt="Meetup" />
+      <img alt="Meetup" width="200" src={logo} />
       <form>
         <input type="email" placeholder="Seu e-mail" />
         <input type="password" placeholder="Sua senha" />
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #7159c1;
+  background: #f1f1f1;
 `
